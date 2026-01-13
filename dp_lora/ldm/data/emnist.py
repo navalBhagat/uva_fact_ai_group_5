13cd0c3167a70792727da669ccffe428b9d90b38
Nico <nico.nap@outlook.com>
2026-01-13 14:13:26 +0100

    feat: add data loader for EMNIST

---
+++ dp_lora/ldm/data/emnist.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+from PIL import Image
+from torchvision import datasets
+
+
+datasets.EMNIST.url = "https://biometrics.nist.gov/cs_links/EMNIST/gzip.zip"
+datasets.EMNIST.filename = "gzip.zip"
+
+
+class EMNISTBase(datasets.EMNIST):
+    def __init__(self, datadir, size=None, split="digits", **kwargs):
+        self.size = size
+        self.split = split
+        cachedir = os.environ.get("TMPDIR", os.path.expanduser("~/.cache"))
+        print(os.path.join(cachedir, datadir))
+        super().__init__(root=os.path.join(cachedir, datadir), split=split, **kwargs)
+
+    def __getitem__(self, index):
+        image, label = super().__getitem__(index)
+
+        img = np.array(image).astype(np.uint8)
+        img_three_channels = np.stack((img, img, img), axis=2)
+
+        img = img_three_channels
+
+        crop = min(img.shape[0], img.shape[1])
+        h, w, = img.shape[0], img.shape[1]
+        img = img[(h - crop) // 2:(h + crop) // 2,
+                  (w - crop) // 2:(w + crop) // 2]
+
+        image = Image.fromarray(img)
+        if self.size is not None:
+            image = image.resize((self.size, self.size))
+
+        image = np.array(image).astype(np.uint8)
+
+        example = {}
+        example["image"] = (image / 127.5 - 1.0).astype(np.float32)
+        example["class_label"] = label
+
+        return example
+
+
+class EMNISTTrain(EMNISTBase):
+    def __init__(self, split="letters", **kwargs):
+        super().__init__(datadir="EMNIST_Train", split=split, train=True, download=True, **kwargs)
+
+
+class EMNISTVal(EMNISTBase):
+    def __init__(self, split="letters", **kwargs):
+        super().__init__(datadir="EMNIST_Val", split=split, train=False, download=True, **kwargs)
